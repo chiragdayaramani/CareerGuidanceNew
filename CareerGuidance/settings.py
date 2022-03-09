2eb1027c6aaaa17bfad1e4da6e0af02c18a0a8bc
Laveena Kithani <71819423+Laveena1208@users.noreply.github.com>
2022-03-09 13:37:11 +0530

    email

--- CareerGuidance/settings.py
+++ CareerGuidance/settings.py
@@ -151,3 +151,10 @@ MESSAGE_TAGS = {
 
 
 SILENCED_SYSTEM_CHECKS = ['mysql.E001']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = #admin email-id (superuser)
+EMAIL_HOST_PASSWORD = #password 
